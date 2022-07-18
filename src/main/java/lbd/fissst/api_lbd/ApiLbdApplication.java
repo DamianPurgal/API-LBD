723af3b7983367f0584fb53782bc45960f455e78
DamianPurgal <damix242@gmail.com>
2022-07-18 13:34:11 +0200

    Task 14,15 - OpenApi + changed title & description

--- src/main/java/lbd/fissst/api_lbd/ApiLbdApplication.java
+++ src/main/java/lbd/fissst/api_lbd/ApiLbdApplication.java
@@ -1,9 +1,21 @@
 package lbd.fissst.api_lbd;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(title = "API - LBD",
+        description = "Some long and useful description :)",
+        version = "v1",
+        license = @License(
+                name = "Apache 2.0",
+                url = "https://www.apache.org/licenses/LICENSE-2.0")
+        )
+)
 public class ApiLbdApplication {
 
     public static void main(String[] args) {
